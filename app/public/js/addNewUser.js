2400b6f843c07fe3d6b121a655bfda0cd7115ca2
Ana Suarez Negreira <ana.suarez.negreira@gmail.com>
2017-02-12 20:51:24 -0600

    updating server.js to remove no needed sections

--- app/public/js/addNewUser.js
+++ app/public/js/addNewUser.js
@@ -14,31 +14,31 @@ $("#search-btn").on("click", function(event) {
  var st1 = document.getElementById("statement1");
  var value1 = st1.options[st1.selectedIndex].value;
 
-  var st2 = document.getElementById("statement1");
+  var st2 = document.getElementById("statement2");
  var value2 = st2.options[st2.selectedIndex].value;
 
-  var st3 = document.getElementById("statement1");
+  var st3 = document.getElementById("statement3");
  var value3 = st3.options[st3.selectedIndex].value;
 
-  var st4 = document.getElementById("statement1");
+  var st4 = document.getElementById("statement4");
  var value4 = st4.options[st4.selectedIndex].value;
 
-  var st5 = document.getElementById("statement1");
+  var st5 = document.getElementById("statement5");
  var value5 = st5.options[st5.selectedIndex].value;
 
-  var st6 = document.getElementById("statement1");
+  var st6 = document.getElementById("statement6");
  var value6 = st6.options[st6.selectedIndex].value;
 
-  var st7 = document.getElementById("statement1");
+  var st7 = document.getElementById("statement7");
  var value7 = st7.options[st7.selectedIndex].value;
 
-  var st8 = document.getElementById("statement1");
+  var st8 = document.getElementById("statement8");
  var value8 = st8.options[st8.selectedIndex].value;
 
-   var st9 = document.getElementById("statement1");
+   var st9 = document.getElementById("statement9");
  var value9 = st9.options[st9.selectedIndex].value;
 
-   var st10 = document.getElementById("statement1");
+   var st10 = document.getElementById("statement10");
  var value10 = st10.options[st10.selectedIndex].value;
 
  answers = [value1, value2, value3, value4, value5, value6, value7, value8, value9, value10];
--- server.js
+++ server.js
@@ -28,56 +28,6 @@ require("./app/routing/htmlRoutes.js")(app);
 // =============================================================
 
 
-// add user into the database and his/her answers
-
-//if you try to visit this in the browser, it will not work cause this is POST
-app.post("/api/new", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    var newreservation = req.body;
-
-    console.log(newreservation);
-
-    // We then add the json the user sent to the character array
-    characters.push(newreservation);
-
-    // We then display the JSON to the users
-    res.json(newreservation);
-});
-
-
-// retrieve information from the database
-
-connection.query("SELECT * FROM newUser",function(err, res) {
-    if (err) throw err;
-    // If we log that user as a JSON, we can see how it looks.
-    console.log(JSON.stringify(res, null, 2));
-});
-
-
-// Search for Specific Character (or all characters) - provides JSON
-app.get("/api/:reservation?", function(req, res) {
-    var chosen = req.params.reservation;
-
-    if (chosen) {
-        console.log("we are here" + chosen);
-
-        for (var i = 0; i < reservation.length; i++) {
-            if (chosen === reservation[i].name) {
-                res.json(reservation[i]);
-                return;
-            }
-        }
-
-        res.json(false);
-    } else {
-        res.json(reservation);
-    }
-});
-
-
-
-
-
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
